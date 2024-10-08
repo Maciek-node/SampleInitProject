172caff40c290561a2bb595255d3355fd7b6837f
Maciek <maciejit3@gmail.com>
2024-10-08 14:24:50 +0200

    Add secret for username

--- helpers/data.js
+++ helpers/data.js
@@ -1,5 +1,5 @@
 import 'dotenv/config'
 export const baseUrl = "https://demoqa.com"
 export const userID="e66a1a48-c80a-4226-bf4d-8771547ae33f"
-export const userName="TesterMaciek"
+export const userName=process.env.SECRET_USERNAME
 export const secretPassword=process.env.SECRET_PASSWORD
