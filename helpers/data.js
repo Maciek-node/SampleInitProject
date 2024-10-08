3bb5bc8e28eaccff58df81609e0457499d347a76
Maciek <maciejit3@gmail.com>
2024-10-08 13:41:22 +0200

    Added new tests with Report

---
+++ helpers/data.js
@@ -0,0 +1,5 @@
+import 'dotenv/config'
+export const baseUrl = "https://demoqa.com"
+export const userID="e66a1a48-c80a-4226-bf4d-8771547ae33f"
+export const userName="TesterMaciek"
+export const secretPassword=process.env.SECRET_PASSWORD
